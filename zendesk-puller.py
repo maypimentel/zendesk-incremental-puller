854bad99153955b425bfee0a0a059bc5e80986d2
Maycon Pimentel <maycon.pimentel@gmail.com>
2020-03-12 23:55:49 -0300

    Refactor on manage_users

--- zendesk-puller.py
+++ zendesk-puller.py
@@ -19,9 +19,11 @@ MONGODB_DB = config('MONGODB_DB', cast=str)
 client = MongoClient(MONGODB_URI)
 db = client[MONGODB_DB]
 try:
-    db.tickets.create_index([('id', ASCENDING)], unique=True)
     db.users.create_index([('id', ASCENDING)], unique=True)
     db.users.create_index([('email', ASCENDING)])
+    db.tickets.create_index([('id', ASCENDING)])
+    db.tickets.create_index([('created_at', ASCENDING)])
+    db.tickets.create_index([('updated_at', ASCENDING)])
 except Exception as err:
     logger.error('Not existis collection: {}'.format(err))
 
@@ -68,7 +70,7 @@ def make_request(startTime=None, cursor=None):
     try:
         qs = {
             'include': 'users',
-            'per_page': 1000,
+            'per_page': 10000,
         }
         if cursor:
             qs['cursor'] = cursor
@@ -84,8 +86,8 @@ def make_request(startTime=None, cursor=None):
             headers={"content-type": "application/json"},
             timeout=SERVICE_REQUEST_TIMEOUT
         )
+        print('Remainnig requests: {}'.format(response.headers['x-rate-limit-remaining']))
         if response.status_code == requests.codes.ok:
-            print(response)
             return response.json()
         elif response.status_code == 429:
             sleep(int(response.headers['retry-after']))
@@ -104,11 +106,11 @@ def manage_users(users):
         if pydash.get(user, 'role') == 'end-user':
             filteredUsers.append({ k: v for (k, v) in user.items() if k in userFields })
     for user in filteredUsers:
-        if db.users.find_one({'id': user.get('id')}):
-            del user['id']
-            db.users.update_one({'id': user.get('id')}, {'$set': user})
-        else:
-            db.users.insert_one(user)
+        if not db.users.find_one({'id': user.get('id')}):
+            try:
+                db.users.insert_one(user)
+            except Exception as err:
+                logger.error('Error: {}'.format(err))
     return filteredUsers
 
 def manage_tickets(tickets):
@@ -124,23 +126,27 @@ def manage_tickets(tickets):
                         if custom['id'] == i:
                             filterTicket[n] = urllib.parse.unquote(custom['value']) if type(custom['value']) is str else custom['value']
             if db.tickets.find_one({'id': filterTicket.get('id')}):
-                db.tickets.update_one({'id': filterTicket.get('id')}, {'$set': filterTicket})
+                try:
+                    db.tickets.update_one({'id': filterTicket.get('id')}, {'$set': filterTicket})
+                except Exception as err:
+                    logger.error('Error: {}'.format(err))
             else:
-                db.tickets.insert_one(filterTicket)
+                try:
+                    db.tickets.insert_one(filterTicket)
+                except Exception as err:
+                    logger.error('Error: {}'.format(err))
         filteredTickets.append(filterTicket)
     return filteredTickets
 
 def main():
     endOfStream = False
     cursor = False
-    startTime = 1583106695
+    startTime = 1583113192
     while not endOfStream:
         response = make_request(startTime, cursor)
         startTime = pydash.get(response, 'end_time')
         cursor = pydash.get(response, 'after_cursor')
         endOfStream = pydash.get(response, 'end_of_stream')
-        # Testing with only one request
-        # endOfStream = True
         users = pydash.get(response, 'users')
         if users and len(users) > 0:
             response['users'] = manage_users(users)
@@ -148,6 +154,6 @@ def main():
         if tickets and len(tickets) >  0:
             response['tickets'] = manage_tickets(tickets)
 
-
 if __name__ == "__main__":
+    print('Begining')
     main()
